38992af73dc398be2a73b26beb39725144fbdd48
Andrew Hall (DEVDIV) <andrehal@microsoft.com>
2017-12-14 16:31:26 -0800

    Updates to runner

--- src/LoadRunner/Program.cs
+++ src/LoadRunner/Program.cs
@@ -1,10 +1,9 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics;
+using System.IO;
 using System.Linq;
-using System.Net;
 using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 using System.Web;
 
@@ -16,9 +15,10 @@ namespace LoadRunner
         const int NumberOfRequests = 30;
         const int SimultaneousRequests = 10;
         const int Min = 1000000;
-        const int Max = 2000000;
-        const bool UseFunction = true;
+        const int Max = 3000000;
+        const bool UseFunction = false;
         const string BaseUrl = "http://localhost:56053/api/Primes?min={0}&max={1}&useFunc={2}";
+        const string OutputFile = "results.csv";
         //static Stopwatch _sw;
 
         static async Task Main(string[] args)
@@ -30,126 +30,44 @@ namespace LoadRunner
             Console.WriteLine("Warmup completed, starting test");
 
             var requests = new RequestCounter(SimultaneousRequests, BaseUrl, Min, Max, UseFunction);
-            var sw = Stopwatch.StartNew();
             var task = requests.MakeRequests(NumberOfRequests);
             var mainPage = new RequestCounter("http://localhost:56053");
-            var mainPageTask = mainPage.MakeRecurringRequest();
+            var mainPageTask = mainPage.MakeRecurringRequest(100, -1);
             await task;
-            sw.Stop();
 
             mainPage.Stop();
             await mainPageTask;
 
             double requestAvg = requests.TotalTime / NumberOfRequests;
+            Console.WriteLine($"Successfully completed {requests.SuccessfulRequests}/{NumberOfRequests} requests, avg/request: {requestAvg}");
+
             double mainPageAvg = mainPage.TotalTime / mainPage.SuccessfulRequests;
-            Console.WriteLine($"Successfully completed {requests.SuccessfulRequests}/{NumberOfRequests} requests in {sw.ElapsedMilliseconds}ms, avg/request: {requestAvg}");
             Console.WriteLine($"Main page was loaded {mainPage.SuccessfulRequests} with an average load time of {mainPageAvg}");
-        }
-    }
 
-    class RequestCounter
-    {
-        object lockObj = new object();
-        private long _totalTime = 0;
-        public long TotalTime
-        {
-            get { lock (lockObj) { return _totalTime; } }
-            set { lock (lockObj) { _totalTime = value; } }
-        }
-        int _madeRequests = 0;
-        public int MadeRequests
-        {
-            get { lock (lockObj) { return _madeRequests; } }
-            set { lock (lockObj) { _madeRequests = value; } }
-        }
-        int _successfulRequests = 0;
-        public int SuccessfulRequests
-        {
-            get { lock (lockObj) { return _successfulRequests; } }
-            set { lock (lockObj) { _successfulRequests = value; } }
+            await PrintResults(OutputFile, new List<RequestCounter>() { requests, mainPage });
         }
 
-        int _simultaneousRequests;
-        string _url;
-
-        CancellationTokenSource _tokenSource;
-
-        public RequestCounter(string url)
+        static async Task PrintResults(string outputFile, List<RequestCounter> counters)
         {
-            _url = url;
-        }
-
-        public RequestCounter(int simultaneousRequests, string url, int min, int max, bool useFunction)
-        {
-            _simultaneousRequests = simultaneousRequests;
-            _url = string.Format(url, min, max, useFunction);
-
-        }
-
-        public void Stop()
-        {
-            if (_tokenSource != null)
+            using (var writer = new StreamWriter(outputFile))
             {
-                _tokenSource.Cancel();
-            }
-        }
-
-        public async Task MakeRecurringRequest()
-        {
-            _tokenSource = new CancellationTokenSource();
-            CancellationToken token = _tokenSource.Token;
-
-            while (!token.IsCancellationRequested)
-            {
-                await MakeRequest();
-                await Task.Delay(1000);
-            }
-        }
-
-        public async Task MakeRequests(int numberOfRequests)
-        {
-            var tasks = new List<Task>();
-
-            for (int i = 0; i < _simultaneousRequests; i++)
-            {
-                var task = NextRequest(numberOfRequests);
-                tasks.Add(task);
-            }
-            await Task.WhenAll(tasks);
-        }
-
-        async Task NextRequest(int totalRequests)
-        {
-            while (MadeRequests < totalRequests)
-            {
-                MadeRequests++;
-                await MakeRequest();
-            }
-        }
-
-        async Task MakeRequest()
-        {
-            Console.WriteLine($"Starting request {MadeRequests}");
-            var sw = Stopwatch.StartNew();
-            try
-            {
-                var req = WebRequest.Create(_url);
-                var resp = await req.GetResponseAsync() as HttpWebResponse;
-                if (resp.StatusCode == HttpStatusCode.OK)
+                await writer.WriteAsync("Time");
+                for (int i = 0; i < counters.Count; i++)
                 {
-                    Console.WriteLine($"Succeeded in {sw.ElapsedMilliseconds}");
-                    lock (lockObj)
+                    await writer.WriteAsync("," + counters[i].Url);
+                }
+                await writer.WriteLineAsync();
+
+                for (int i = 0; i < counters.Count; i++)
+                {
+                    var padding = new string(',', i + 1);
+
+                    foreach (var record in counters[i].RequestHistory)
                     {
-                        SuccessfulRequests++;
+                        await writer.WriteLineAsync(record.TimeStamp + padding + record.Duration);
                     }
                 }
             }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Request failed: " + ex.Message);
-            }
-            sw.Stop();
-            TotalTime += sw.ElapsedMilliseconds;
         }
     }
 }
---
+++ src/LoadRunner/RequestCounter.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LoadRunner
+{
+    class RequestRecord
+    {
+        public DateTime TimeStamp { get; set; }
+        public string Url { get; set; }
+        public long Duration { get; set; }
+    }
+
+    class RequestCounter
+    {
+        public List<RequestRecord> RequestHistory = new List<RequestRecord>();
+
+        object lockObj = new object();
+        private long _totalTime = 0;
+        public long TotalTime
+        {
+            get { lock (lockObj) { return _totalTime; } }
+            set { lock (lockObj) { _totalTime = value; } }
+        }
+        int _madeRequests = 0;
+        public int MadeRequests
+        {
+            get { lock (lockObj) { return _madeRequests; } }
+            set { lock (lockObj) { _madeRequests = value; } }
+        }
+        int _successfulRequests = 0;
+        public int SuccessfulRequests
+        {
+            get { lock (lockObj) { return _successfulRequests; } }
+            set { lock (lockObj) { _successfulRequests = value; } }
+        }
+
+        int _simultaneousRequests;
+        public string Url { get; set; }
+
+        CancellationTokenSource _tokenSource;
+
+        public RequestCounter(string url)
+        {
+            Url = url;
+        }
+
+        public RequestCounter(int simultaneousRequests, string url, int min, int max, bool useFunction)
+        {
+            _simultaneousRequests = simultaneousRequests;
+            Url = string.Format(url, min, max, useFunction);
+
+        }
+
+        public void Stop()
+        {
+            if (_tokenSource != null)
+            {
+                _tokenSource.Cancel();
+            }
+        }
+
+        public async Task MakeRecurringRequest(int delay, int maxCount = -1)
+        {
+            _tokenSource = new CancellationTokenSource();
+            CancellationToken token = _tokenSource.Token;
+            int reqCount = 0;
+
+            while (!token.IsCancellationRequested)
+            {
+                await MakeRequest();
+                await Task.Delay(delay);
+                reqCount++;
+                if (maxCount > 0 && reqCount == maxCount)
+                {
+                    _tokenSource.Cancel();
+                }
+            }
+        }
+
+        public async Task MakeRequests(int numberOfRequests)
+        {
+            var tasks = new List<Task>();
+
+            for (int i = 0; i < _simultaneousRequests; i++)
+            {
+                var task = NextRequest(numberOfRequests);
+                tasks.Add(task);
+            }
+            await Task.WhenAll(tasks);
+        }
+
+        async Task NextRequest(int totalRequests)
+        {
+            while (MadeRequests < totalRequests)
+            {
+                MadeRequests++;
+                await MakeRequest();
+            }
+        }
+
+        async Task MakeRequest()
+        {
+            Console.WriteLine($"Starting request {MadeRequests}");
+            var begin = DateTime.Now;
+            var sw = Stopwatch.StartNew();
+            try
+            {
+                var req = WebRequest.Create(Url);
+                var resp = await req.GetResponseAsync() as HttpWebResponse;
+                if (resp.StatusCode == HttpStatusCode.OK)
+                {
+                    Console.WriteLine($"Succeeded in {sw.ElapsedMilliseconds}");
+                    SuccessfulRequests++;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Request failed: " + ex.Message);
+            }
+            sw.Stop();
+            RequestHistory.Add(new RequestRecord() { TimeStamp = begin, Url = Url, Duration = sw.ElapsedMilliseconds});
+            TotalTime += sw.ElapsedMilliseconds;
+        }
+    }
+}
